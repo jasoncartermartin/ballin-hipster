490758a0040f42cdbf66936dac5b2f107f373570
Jason Martin <jason.carter.martin@gmail.com>
2014-01-28 18:54:29 -0800

    Fixed javascript preview issue

--- app/assets/javascripts/event_handlers.js
+++ app/assets/javascripts/event_handlers.js
@@ -1,3 +1,4 @@
+/*
 $("#wiki_body").keyup(function (){
   $("#wiki_body_preview").text(function (){
     $("#wiki_body").attr('data-value', wiki_body.value);
@@ -8,4 +9,10 @@ $("#wiki_body").keyup(function (){
   });
   alert( $("#wiki_body").text() );
 });
-
+*/
+$("#wiki_body").keyup(function (){
+  $("#wiki_body_preview").text(function (){
+    return $("#wiki_body").val();
+    });
+  //alert( $("#wiki_body").val() );
+});
