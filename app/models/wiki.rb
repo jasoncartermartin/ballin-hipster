35f9b0e1d4942b54cc1f19005074b3178f28b107
Jason Martin <jason.carter.martin@gmail.com>
2014-02-05 15:49:55 -0800

    Scoped users without a pro account to not be able to see private wikis on the wiki view

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -6,6 +6,6 @@ class Wiki < ActiveRecord::Base
   accepts_nested_attributes_for :user
   accepts_nested_attributes_for :collaborators
 
-  scope :visible_to, lambda { |user| user ? scoped : where(private: false) }
+  scope :visible_to, lambda { |user| user.premium? ? scoped : where(private: false) }
 
 end
