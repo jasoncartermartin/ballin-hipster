fbb037808f0d6c0d927804b3d30cc643ce05300b
Jason Martin <jason.carter.martin@gmail.com>
2014-02-06 16:58:02 -0800

    Completed Epic Editor integration, configured all functionality and tested working

--- app/assets/javascripts/event_handlers.js
+++ app/assets/javascripts/event_handlers.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-  $("#wiki_body").bind('input propertychange', function (){
-    $("#wiki_body_preview").text(function (){
-      return $("#wiki_body").val();
-    });
-  });
-});
+// ** This Javascript has been rendered unnecessary due to Epic Editor
+
+// $(document).ready(function() {
+//   $("#wiki_body").bind('input propertychange', function (){
+//     $("#wiki_body_preview").text(function (){
+//       return $("#wiki_body").val();
+//     });
+//   });
+// });
