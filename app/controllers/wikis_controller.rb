8605b269fb285e6314d8a69b9f62e9f31a70420c
Jason Martin <jason.carter.martin@gmail.com>
2014-02-11 12:34:28 -0800

    Renamed to Wiki on Rails, removed all code in view down to controller & model, disabled collaborator if they are owner of wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,54 +1,72 @@
 class WikisController < ApplicationController
   before_action :set_wiki, only: [:edit, :update, :destroy]
 
-  
-
-  # GET /wikis
-  # GET /wikis.json
   def index
+    
     @wikis = Wiki.visible_to(current_user)
+  
   end
 
-  # GET /wikis/1
-  # GET /wikis/1.json
   def show
+
     @wiki = Wiki.friendly.find(params[:id])
+    
     if request.path != wiki_path(@wiki)
       redirect_to @wiki, status: :moved_permanently
     end
-    authorize @wiki
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
-  end
-
-  # GET /wikis/new
-  def new
-    @wiki = Wiki.new
-  end
-
-  # GET /wikis/1/edit
-  def edit
-    authorize @wiki
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
-  end
-
-  # POST /wikis
-  # POST /wikis.json
-  def create
     
+    authorize @wiki
+    
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
+  
+  end
+
+  def new
+  
+    @wiki = Wiki.new 
+    
+    @wiki.user_id = current_user.id
+    @created_by = @wiki.user
+
+
+  end
+
+  def edit
+
+    authorize @wiki
+  
+    @created_by
+    if @wiki.user
+      @created_by = @wiki.user
+    else
+      @created_by = current_user
+    end
+
+    @currentCollaborators = []
+    
+    @wiki.collaborators.each do |c|
+      @currentCollaborators << c.user.id
+    end 
+    
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
+  
+  end
+
+  def create
+  
+    ## Custom code (collab_list) needed to support chosen multi-select dropdown with nested model
+
     collab_list = params[:wiki][:collab_list] || []
     params[:wiki].delete :collab_list
     
     @wiki = Wiki.new(wiki_params)
     @wiki.user_id = current_user.id
-    
-    message = ""
 
     respond_to do |format|
       if @wiki.save
           removedCollaborators = @wiki.collaborators.all - @wiki.collaborators.where(user_id: collab_list)
           
           removedCollaborators.each do |r|
-            message << r.user.email + " has been removed as a collaborator, "
             r.delete
           end
 
@@ -66,21 +84,21 @@ class WikisController < ApplicationController
         format.json { render json: @wiki.errors, status: :unprocessable_entity }
       end
     end
+ 
   end
 
-  # PATCH/PUT /wikis/1
-  # PATCH/PUT /wikis/1.json
   def update
+
     authorize @wiki
+
+    ## Custom code (collab_list) needed to support chosen multi-select dropdown with nested model
+
     collab_list = params[:wiki][:collab_list] || []
     params[:wiki].delete :collab_list
-
-    message = "" 
 
     removedCollaborators = @wiki.collaborators.all - @wiki.collaborators.where(user_id: collab_list)
     
     removedCollaborators.each do |r|
-      message << r.user.email + " has been removed as a collaborator, "
       r.delete
     end
 
@@ -92,27 +110,29 @@ class WikisController < ApplicationController
     
     respond_to do |format|
       if @wiki.update(wiki_params)
-        format.html { redirect_to @wiki, notice: "Record saved! #{message}" }
+        format.html { redirect_to @wiki, notice: "Wiki saved!" }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
         format.json { render json: @wiki.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
-  # DELETE /wikis/1
-  # DELETE /wikis/1.json
   def destroy
+
     authorize @wiki
     @wiki.destroy
+    
     respond_to do |format|
       format.html { redirect_to wikis_url }
       format.json { head :no_content }
     end
+
   end
 
-  def should_generate_new_friendly_id?
+  def should_generate_new_friendly_id? # For FriendlyID GEM 
     true
   end
 
@@ -126,5 +146,5 @@ class WikisController < ApplicationController
     def wiki_params
       params.require(:wiki).permit(:title, :body, :private, :image, :user_id, collab_list: params[:collab_list])
     end
-    #params.require(:wiki).permit(:title, :body, :user_id, :private, collaborators_attributes: [collaborator: [:user_id]])
+
 end
